7e4bf14e908ae5068e17b9ff779eed1709696c05
Lucas Lago <lucasbenderlago@gmail.com>
2016-08-12 16:53:00 -0300

    validate json web token

--- express-app/config/app-config.js
+++ express-app/config/app-config.js
@@ -2,8 +2,6 @@ module.exports = {
   OKTA_REDIRECT_URI: 'http://localhost:8080/oauth/callback',
   OKTA_CLIENT_ID: '90WMmOm0btALpmbOVdlx',
   OKTA_BASE_URL: 'https://thoughtworks.oktapreview.com',
-  OKTA_PUBLIC_KEYS: {
-
-  },
+  OKTA_PUBLIC_KEYS: {"alg":"RS256","e":"AQAB","n":"3DCG18npGYaVaTDYdzOnKV5KeOgaQoKlbT5lBzQn7vqfGW2xCfH9aviBgYeSU200dLqWoDQuE3nI5GuSsck5kCIYsH78qWYZpvQVGACBchYuO1S5m31iNlLeEjBumc40A2OtlVP8UrHByv6n8k276h7WT2VylK4JGOr4rLBkujybZUXWdaPhgy5QNs8HMPKIPw0EH3rEfW9ycjpy36tvbEmtAfSsgmpCaC66MSdDV7Ep41NYV9fpl-CwnpSoj2WSd_QBIhrZVnK_IC8HBgKk8FsNJZ2qjQk66auXNplyVfLHh4RE_TbGx9LOiYPkNss6RJO6rWY5K4wW9lbG-6v7mQ","kid":"UhWYS3USL4yKxVV1A0uTn6R3Q5pcMya7q4jQ5bd9gDk","kty":"RSA","use":"sig"},
   SESSION_SECRET: 'super secret!'
 };
--- express-app/routes/home.js
+++ express-app/routes/home.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-  res.render('index', {title: 'Demo'});
+  res.render('index', {user_email: req.session.user, title: 'Demo Express App'});
 });
 
 module.exports = router;
